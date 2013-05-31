b7f37ce6b4fd042bec915d3e6bf847fd44119ce4
woojj12 <woojj12@gmail.com>
2013-06-01 01:00:34 +0900

    Update delta_ftl_frame.c

--- ftl_delta/delta_ftl_frame.c
+++ ftl_delta/delta_ftl_frame.c
@@ -33,6 +33,100 @@ put_delta();			//put delta data in delta_write_buffer
 //functions
 //read stream function
 
+static void format(void)
+{
+    uart_printf("Total FTL DRAM metadata size: %d KB", DRAM_BYTES_OTHER / 1024);
+
+	/*
+    uart_print("do format");
+    uart_print("NUM_PSECTORS");
+    uart_print_32(NUM_PSECTORS);
+    uart_print("NUM_LSECTORS");
+    uart_print_32(NUM_LSECTORS);
+    uart_print("VBLKS_PER_BANK");
+    uart_print_32(VBLKS_PER_BANK);
+    uart_print("DATA_BLK_PER_BANK");
+    uart_print_32(DATA_BLK_PER_BANK);
+    uart_print("LOG_BLK_PER_BANK");
+    uart_print_32(LOG_BLK_PER_BANK);
+    uart_print("ISOL_BLK_PER_BANK");
+    uart_print_32(ISOL_BLK_PER_BANK);
+    uart_print("FREE_BLK_PER_BANK");
+    uart_print_32(FREE_BLK_PER_BANK);
+	*/
+
+    //----------------------------------------
+    // initialize DRAM metadata
+    //----------------------------------------
+    // data/log/isolation/free block mapping table
+    mem_set_dram(DATA_PMT_ADDR, NULL, DATA_PMT_BYTES);
+	mem_set_dram(DELTA_PMT_ADDR, NULL, DELTA_PMT_BYTES);
+    mem_set_dram(RSRV_BMT_ADDR, NULL, RSRV_BMT_BYTES);
+
+    // setting map/data/log/isolation/free block mapping table
+    // NOTE: exclude bad blocks
+    UINT32 lbn, vblock;
+    for (UINT32 bank = 0; bank < NUM_BANKS; bank++) {
+        vblock = MISCBLK_VBN;
+
+        // misc. block (fixed location)
+        nand_block_erase(bank, vblock);
+
+        g_bsp_isr_flag[bank] = INVALID;
+        // map block
+        for (lbn = 0; lbn < MAP_BLK_PER_BANK;) {
+            vblock++;
+            if (is_bad_block(bank, vblock) == TRUE) {
+                continue;
+            }
+            nand_block_erase_sync(bank, vblock);
+            if (g_bsp_isr_flag[bank] != INVALID) {
+                set_bad_block(bank, g_bsp_isr_flag[bank]);
+                g_bsp_isr_flag[bank] = INVALID;
+                continue;
+            }
+            set_mapblk_vpn(bank, lbn, vblock * PAGES_PER_BLK - 1);
+            lbn++;
+        }
+        // rsrv block mapping table
+        for (lbn = 0; lbn < RSRV_BLK_PER_BANK;) {
+            vblock++;
+            if (vblock >= VBLKS_PER_BANK) {
+                break;
+            }
+            if (is_bad_block(bank, vblock) == TRUE) {
+                continue;
+            }
+            nand_block_erase_sync(bank, vblock);
+            if (g_bsp_isr_flag[bank] != INVALID) {
+                set_bad_block(bank, g_bsp_isr_flag[bank]);
+                g_bsp_isr_flag[bank] = INVALID;
+                continue;
+            }
+            ret_free_vbn(bank, vblock);
+            lbn++;
+        }
+        uart_printf("above log blocks are invalid..bank %d lbn %d", bank, lbn);
+        // set remained rsrv blocks as `invalid'
+        while (lbn < LOG_BLK_PER_BANK) {
+            write_dram_16(LOG_BMT_ADDR + ((bank * LOG_BLK_PER_BANK + lbn) * sizeof(UINT16)),
+                          (UINT16)-1);
+            lbn++;
+        }
+    }
+    //----------------------------------------
+    // initialize SRAM metadata
+    //----------------------------------------
+    init_metadata_sram();
+
+    // flush FTL metadata into NAND flash
+    ftl_flush();
+
+    write_format_mark();
+	led(1);
+    uart_print("format complete");
+}
+
 ftl_read()
 {
   if(is_in_write_buffer())	//is in write buffer?
