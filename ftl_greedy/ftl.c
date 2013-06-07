773ffc1511f80cfb15f25773f7c3217d45bfed01
woojj12 <woojj12@gmail.com>
2013-06-08 05:49:58 +0900

    Update ftl.c

--- ftl_greedy/ftl.c
+++ ftl_greedy/ftl.c
@@ -39,10 +39,10 @@
 // the number of sectors of misc. metadata info.
 #define NUM_MISC_META_SECT  ((sizeof(misc_metadata) + BYTES_PER_SECTOR - 1)/ BYTES_PER_SECTOR)
 #define NUM_VCOUNT_SECT     ((VBLKS_PER_BANK * sizeof(UINT16) + BYTES_PER_SECTOR - 1) / BYTES_PER_SECTOR)
-#define NUM_COMP_CLUSTER		16
+#define NUM_COMP_CLUSTER		32
 #define CLUSTER_SIZE			(BYTES_PER_PAGE / NUM_COMP_CLUSTER)
 #define ALIGN32(data)			((data + sizeof(UINT32) - 1) / sizeof(UINT32) * sizeof(UINT32))
-#define MAX_COMPRESS_SIZE		(CLUSTER_SIZE / 2)				//2048		//MAX COMPRESS SIZE : 2K(50%)
+#define MAX_COMPRESS_SIZE		(BYTES_PER_PAGE / 2)				//2048		//MAX COMPRESS SIZE : 2K(50%)
 
 /////////////////////////////////////////////
 
@@ -164,7 +164,7 @@ static void xor_buffer(UINT32 const src0, UINT32 const src1, UINT32 const dst);
 static UINT32 find_delta_data(UINT32 const buf_ptr, UINT32 const delta_ppa);		//find delta data in temp_buffer;
 static void _lzf_decompress (UINT32 const in_data_base, UINT32 const out_data_base);		//decompress data
 static UINT32 _lzf_compress (UINT32 const in_data_base, UINT32 const out_data_base);		//compress by lzf
-static UINT32 is_remain_delta_buffer(UINT32 const bank, UINT32 const cs);	//is remain in delta_write_buffer?
+static UINT32 is_remain_delta_buffer(UINT32 const bank, UINT16 const cs);	//is remain in delta_write_buffer?
 
 static void sanity_check(void)
 {
@@ -380,18 +380,20 @@ void ftl_read(UINT32 const lba, UINT32 const num_sectors)
 
 		if (vpn != NULL)
 		{
-			//?댁쟻???덉쓬
-			/* ?명?媛 ?덉쑝硫?			 * 	?명?瑜??쎌뼱????⑹튇?ㅼ쓬 由щ뱶踰꾪띁???섍?
-			 * ?명?媛 ?놁쑝硫?			 * 	洹몃깷 由ы꽩
+			//쓴적이 있음
+			/* 델타가 있으면
+			 * 	델타를 읽어와서 합친다음 리드버퍼에 넘김
+			 * 델타가 없으면
+			 * 	그냥 리턴
 			 */
 			if(is_valid_ppa(vpn) == FALSE)
 			{
-				//?명?媛 ?덉쓬
+				//델타가 있음
 				merge(bank, lpn, get_ppa_delta(lpn), RD_BUF_PTR(g_ftl_read_buf_id));
 			}
 			else
 			{
-				//?명?媛 ?놁쓬
+				//델타가 없음
 				nand_page_read(bank,
 						vpn / PAGES_PER_BLK,
 						vpn % PAGES_PER_BLK,
@@ -435,9 +437,10 @@ void ftl_write(UINT32 const lba, UINT32 const num_sectors)
 	lpn          = lba / SECTORS_PER_PAGE;
 	sect_offset  = lba % SECTORS_PER_PAGE;
 	remain_sects = num_sectors;
-
+uart_printf("ftl_write");
 	while (remain_sects != 0)
 	{
+
 		if ((sect_offset + remain_sects) < SECTORS_PER_PAGE)
 		{
 			num_sectors_to_write = remain_sects;
@@ -470,8 +473,9 @@ static void write_page(UINT32 const lpn, UINT32 const sect_offset, UINT32 const 
 	old_vpn  = get_vpn(lpn);
 	/*
 	 * old_vpn
-	 * 泥섏쓬?곕㈃ ?먯씠怨?	 * ?명?媛 ?덉쑝硫??몃갭由щ뱶
-	 * ?명?媛 ?놁쑝硫?諛몃━?쒓쿋吏
+	 * 처음쓰면 널이고
+	 * 델타가 있으면 인밸리드
+	 * 델타가 없으면 밸리드겠지
 	 */
 
 	g_ftl_statistics[bank].page_wcount++;
@@ -479,6 +483,7 @@ static void write_page(UINT32 const lpn, UINT32 const sect_offset, UINT32 const 
 	// if old data already exist,
 	if (old_vpn != NULL)
 	{
+uart_printf("oldvpn notnull");
 		old_vpn = set_valid_ppa(old_vpn);
 
 		vblock   = old_vpn / PAGES_PER_BLK;
@@ -502,54 +507,62 @@ static void write_page(UINT32 const lpn, UINT32 const sect_offset, UINT32 const 
 			if ((page_offset + column_cnt) < SECTORS_PER_PAGE)
 			{
 				UINT32 const rhole_base = (page_offset + num_sectors) * BYTES_PER_SECTOR;
-				mem_copy(WR_BUF_PTR(g_ftl_write_buf_id) + rhole_base, FTL_BUF(bank), BYTES_PER_PAGE - rhole_base);
+				mem_copy(WR_BUF_PTR(g_ftl_write_buf_id) + rhole_base, FTL_BUF(bank) + rhole_base, BYTES_PER_PAGE - rhole_base);
 			}
 		}
 		// full page write
 		page_offset = 0;
 		column_cnt  = SECTORS_PER_PAGE;
 
-		//BOOL32 comp_success = compress(TEMP_BUF_PTR(0), WR_BUF_PTR(g_ftl_write_buf_id));
-		//if(comp_success == TRUE)
-		if( FALSE )
+		BOOL32 comp_success = compress(TEMP_BUF_PTR(0), WR_BUF_PTR(g_ftl_write_buf_id));
+		if(comp_success == TRUE)
+//		if( FALSE )
 		{
 			uart_printf("evict 3\n");
-			//?뺤텞 ?깃났
-			//?곗씠?곕ℓ?묓뀒?대툝 ?낅뜲?댄듃
+			
+
+			//압축 성공
+			//데이터매핑테이블 업데이트
 			old_vpn = set_invalid_ppa(old_vpn);
 			set_vpn(lpn, old_vpn);
-			//?명?留ㅽ븨 ???꾩튂?좎젙
+
+			//델타매핑 쓸 위치선정
 			UINT32 delta_page_map_offset = get_next_delta_table_space(bank, lpn);
 
 			/*
-			 * ?명? 留ㅽ븨 ?뚯씠釉붿뿉?ㅺ? ?뷀듃由??낅뜲?댄듃
-			 * lpa??洹몃깷 ??二쇨퀬
-			 * ppa??0???
-			 * 0? ?명? 踰꾪띁???덈떎???섎?
-			 * 洹몃━怨??뺤텞??寃껋? write to delta
+			 * 델타 매핑 테이블에다가 엔트리 업데이트
+			 * lpa는 그냥 써 주고
+			 * ppa는 0을 씀
+			 * 0은 델타 버퍼에 있다는 의미
+			 * 그리고 압축한 것은 write to delta
 			 */
 			set_delta_lpa(delta_page_map_offset, lpn);
 			set_delta_ppa(delta_page_map_offset, 0);
 			/*
-			 * ?명????곕뒗 ?⑥닔
+			 * 델타에 쓰는 함수
 			 * write_to_delta
-			 * ?몄옄 : bank, lpa, buf_ptr
-			 * buf_ptr? ?뺤텞???덉씠 ??λ맂 踰꾪띁
-			 * ?뺤텞???덉쓣 ?명?踰꾪띁??吏묒뼱?ｌ쓬
-			 * 苑됱감硫??뚯븘???몃뱶???
+			 * 인자 : bank, lpa, buf_ptr
+			 * buf_ptr은 압축한 놈이 저장된 버퍼
+			 * 압축한 놈을 델타버퍼에 집어넣음
+			 * 꽉차면 알아서 낸드에 씀
 			 */
+
 			write_to_delta(bank, lpn, TEMP_BUF_PTR(2));
+
 			return;
 		}
 		else
 		{
+			uart_printf("evict 4");
 			// invalid old page (decrease vcount)
 			set_vcount(bank, vblock, get_vcount(bank, vblock) - 1);
 		}
 	}
 	else
 	{
-		//泥섏쓬 ?
+uart_printf("write initial");
+uart_printf("aaaa");
+		//처음 씀
 		if (num_sectors != SECTORS_PER_PAGE)
 		{
 			if(page_offset != 0)
@@ -564,13 +577,16 @@ static void write_page(UINT32 const lpn, UINT32 const sect_offset, UINT32 const 
 	new_vpn  = assign_new_write_vpn(bank);
 	vblock   = new_vpn / PAGES_PER_BLK;
 	page_num = new_vpn % PAGES_PER_BLK;
+uart_printf("bbbb");
 	ASSERT(get_vcount(bank,vblock) < (PAGES_PER_BLK - 1));
-
+uart_printf("nand_pre");
 	// write new data (make sure that the new data is ready in the write buffer frame)
 	// (c.f FO_B_SATA_W flag in flash.h)
 	nand_page_program_from_host(bank,
 			vblock,
 			page_num);
+uart_printf("nand_next");
+
 	// update metadata
 	set_lpn(bank, page_num, lpn);
 	set_vpn(lpn, new_vpn);
@@ -837,6 +853,16 @@ static void init_metadata_sram(void)
 	UINT32 vblock;
 	UINT32 mapblk_lbn;
 
+
+	g_delta_pmt_pointer = -1;
+	uart_printf("BYTES_PER_PAGE : %x", BYTES_PER_PAGE);
+	for (bank = 0; bank < NUM_BANKS; bank++)
+	{
+		next_delta_offset[bank] = DELTA_BUF(bank) + (1 + 2 * MAX_DELTAS_PER_PAGE) * sizeof(UINT32);
+		next_delta_meta[bank] = DELTA_BUF(bank) + sizeof(UINT32);
+		uart_printf("DELTA_BUF(%x) = %x", bank, DELTA_BUF(bank));
+	}
+
 	//----------------------------------------
 	// initialize misc. metadata
 	//----------------------------------------
@@ -892,7 +918,7 @@ static void init_metadata_sram(void)
 		do
 		{
 			vblock++;
-			// 占쎄쑴??next vblock?븝옙苑?占쎈뜄以덌옙占쏙옙怨쀬뵠占쎄퀡占?占쏙옙?ｏ옙占쏙옙?뽰삂
+			//
 			set_new_write_vpn(bank, vblock * PAGES_PER_BLK);
 			ASSERT(vblock < VBLKS_PER_BANK);
 		}while(is_bad_block(bank, vblock) == TRUE);
@@ -1291,8 +1317,9 @@ static BOOL32 is_valid_ppa(UINT32 ppa)
 		return TRUE;
 }
 
-/*lpa瑜?諛쏆븘???명? 留ㅽ븨???ㅼ졇?? * ?명? 留ㅽ븨???ㅽ봽?뗭쓣 諛섑솚
- * ?놁쑝硫??몃갭
+/*lpa를 받아서 델타 매핑을 뒤져서
+ * 델타 매핑의 오프셋을 반환
+ * 없으면 인밸
  */
 static UINT32 get_delta_map_offset(UINT32 const lpa)
 {
@@ -1307,21 +1334,26 @@ static UINT32 get_delta_map_offset(UINT32 const lpa)
 	return INVAL;
 }
 
-//?뗭틦?쒕? ?덉벐寃??섎㈃??//merge?섎뒗 ?곹솴???명? 留ㅽ븨????李쇱쓣?뚮옉
-//read ?????⑹튇 媛믪쓣 ?섍꺼二쇰뒗 ?곹솴 ?먭?吏??//lpa???명? ppa留??몄옄濡?二쇰㈃
-//?섎㉧吏???ш린???뚯븘??//踰꾪띁???쇰떒 ?쒗봽踰꾪띁濡??좉쾶
-//?섏쨷??肄붾뱶 ??吏꾪뻾?섏빞 ?쒗봽踰꾪띁 ?⑤룄 ?섎뒗吏 紐살벐?붿? ?????덉쓣寃?媛숈븘
+//ㅋ캐시를 안쓰게 되면서
+//merge하는 상황이 델타 매핑이 다 찼을때랑
+//read 할 때 합친 값을 넘겨주는 상황 두가지임
+//lpa랑 델타 ppa만 인자로 주면
+//나머지는 여기서 알아서
+//버퍼는 일단 템프버퍼로 할게
+//나중에 코드 더 진행되야 템프버퍼 써도 되는지 못쓰는지 알 수 있을것 같아
 static void merge(UINT32 const bank, UINT32 const lpa, UINT32 const ppa_delta, UINT32 const buf_ptr)
 {
 	ASSERT(ppa_delta != INVAL);
 	UINT32 ppa_ori = set_valid_ppa(get_vpn(lpa));
 
-	//?명????ㅻ━吏???쎌뼱??	read_from_delta(bank, lpa, ppa_delta, TEMP_BUF_PTR(1));
+	//델타랑 오리지널 읽어옴
+	read_from_delta(bank, lpa, ppa_delta, TEMP_BUF_PTR(1));
 	nand_page_read(bank, get_pbn(ppa_ori), get_offset(ppa_ori), TEMP_BUF_PTR(0));
 
 	//read_from_delta????
 
-	//xor ?댁꽌 ?쒗봽踰꾪봽0?????	xor_buffer(TEMP_BUF_PTR(0), TEMP_BUF_PTR(1), buf_ptr);
+	//xor 해서 템프버프0에 저장
+	xor_buffer(TEMP_BUF_PTR(0), TEMP_BUF_PTR(1), buf_ptr);
 }
 
 static void read_from_delta(UINT32 const bank, UINT32 const lpa, UINT32 const delta_ppa, UINT32 const dst_buf)		//read delta to buf_addr
@@ -1335,39 +1367,43 @@ static void read_from_delta(UINT32 const bank, UINT32 const lpa, UINT32 const de
 
 	if(delta_ppa == NULL)
 	{
+		uart_printf("buf_ptr : DELTA_BUF(BANK)");
 		buf_ptr = DELTA_BUF(bank);
 	}
 	else
 	{
+		uart_printf("buf_ptr : DELTA_TEMP_BUF_PTR(0)");
 		nand_page_read(bank, pbn, offset, DELTA_TEMP_BUF_PTR(0));	//load from nand to temp_buffer
 		buf_ptr = DELTA_TEMP_BUF_PTR(0);
 	}
 
 	delta_read_start = find_delta_data(buf_ptr, lpa);		//find delta data in temp_buffer;
+uart_printf("delta_read_start %x", delta_read_start);
 	_lzf_decompress(delta_read_start, dst_buf);
 
 	return;
 }
 
 /*
-* ?뺤텞?섎뒗 ?⑥닔
+* 압축하는 함수
 * compress
-* ?몄옄 : 踰꾪띁 ?먭컻
-* 由ы꽩 : TRUE/FALSE
-* ?깃났?섎㈃ TEMP_BUF_PTR(2)???뺤텞??寃껋씠 ?ㅼ뼱媛?*/
-//buf_data : ?먮옒 nand???덈뜕 ?섏씠吏, buf_write : write???덈줈 ?곗씠???섏씠吏
+* 인자 : 버퍼 두개
+* 리턴 : TRUE/FALSE
+* 성공하면 TEMP_BUF_PTR(2)에 압축한 것이 들어감
+*/
+//buf_data : 원래 nand에 있던 페이지, buf_write : write시 새로 쓰이는 페이지
 //BOOL32 compress(TEMP_BUF_PTR(0), WR_BUF_PTR(g_ftl_write_buf_id))
 static BOOL32 compress(UINT32 buf_data, UINT32 buf_write)
 {
 	BOOL32 success;
 
-	//buf_data, buf_write瑜??뺤텞?댁꽌 TMP_BUF_PTR(1)濡?蹂대궡以??
+	//buf_data, buf_write를 압축해서 TMP_BUF_PTR(1)로 보내준다.
 	//xor_buffer(UINT32 const src0, UINT32 const src1, UINT32 const dst);
 	xor_buffer(buf_data, buf_write, TEMP_BUF_PTR(1));
 	uart_printf("xor_buffer");
 
-	//TEMP_BUF_PTR(1)?먯꽌 ?뺤텞?섏뿬 TEMP_BUF_PTR(2)濡?蹂대궡以??
-	//success??_lzf_compress媛 ?깃났?곸쑝濡??섏뿀?붿?瑜??뚮젮以??
+	//TEMP_BUF_PTR(1)에서 압축하여 TEMP_BUF_PTR(2)로 보내준다.
+	//success는 _lzf_compress가 성공적으로 되었는지를 알려준다.
 	success = (_lzf_compress(TEMP_BUF_PTR(1), TEMP_BUF_PTR(2)) > 0);
 
 	uart_printf("lzf_compress : %d", success);
@@ -1380,7 +1416,6 @@ static UINT32 get_next_delta_table_space(UINT32 const bank, UINT32 const lpa)
 	UINT32 offset;
 	UINT32 new_ppa = 0;
 GET_NEXT_DELTA_TABLE_SPACE_START:
-
 	offset = (g_delta_pmt_pointer + 1) % NUM_MAX_DELTA_PAGES;
 	//g_delta_pmt_pointer = (g_delta_pmt_pointer + 1) % NUM_MAX_DELTA_PAGES;
 	UINT32 old_delta_ppa = get_delta_ppa(offset);
@@ -1388,61 +1423,81 @@ GET_NEXT_DELTA_TABLE_SPACE_START:
 
 	if(old_delta_lpa == lpa)
 	{
+		uart_printf("case1");
+		
 		/*
-		 * 吏湲?媛由ы궓 ?덉씠 ?먭린 ?먯떊???ㅼ뼱?덈뒗 ?뷀듃由?		 * 洹몃깷 ??뼱 ?
+		 * 지금 가리킨 놈이 자기 자신이 들어있는 엔트리
+		 * 그냥 덮어 씀
 		 */
 		goto GET_NEXT_DELTA_TABLE_SPACE_END;
 	}
 	else
 	{
+		uart_printf("case2");
+		
 		/*
-		 * ?먭린 ?뷀듃由ш? ?꾨떂
+		 * 자기 엔트리가 아님
 		 */
 		if(old_delta_ppa == 0)
 		{
+			uart_printf("case2-1");
 			/*
-			 * 吏湲?媛由ы궓 ?덉씠 ?섑븘 踰꾪띁???덈뒗?덉엫
-			 * ?섎? 鍮쇨퀬 ?댁찈怨??섎㈃ 蹂듭옟??吏?덇퉴
-			 * 洹몃깷 ?ㅼ쓬?먯꽌 李얜룄濡??섏옄
+			 * 지금 가리킨 놈이 하필 버퍼에 있는놈임
+			 * 얘를 빼고 어쩌고 하면 복잡해 지니까
+			 * 그냥 다음에서 찾도록 하자
 			 */
 			g_delta_pmt_pointer = offset;
 			goto GET_NEXT_DELTA_TABLE_SPACE_START;
 		}
 		else if(is_valid_ppa(old_delta_ppa) == FALSE)
 		{
+			uart_printf("case2-2");
 			/*
-			 * 吏湲??ъ씤?곌? 媛由ы궎???뷀듃由ш? ?몃갭由щ뱶???덉씠硫?			 * 洹??꾩튂?먮떎 洹몃깷 ??뼱 ?곕㈃ ??			 */
+			 * 지금 포인터가 가리키는 엔트리가 인밸리드한 놈이면
+			 * 그 위치에다 그냥 덮어 쓰면 됨
+			 */
 			/*
-			 * ?몃갭由щ뱶 ???덉씠?쇰뒗嫄?			 * 洹??명? 留ㅽ븨 ?뚯씠釉?移몄뿉 泥섏쓬 ?곕뒗嫄곕? ?섎?!
+			 * 인밸리드 한 놈이라는건
+			 * 그 델타 매핑 테이블 칸에 처음 쓰는거란 의미!
 			 */
 			goto GET_NEXT_DELTA_TABLE_SPACE_END;
 		}
 
 		else
 		{
+			uart_printf("case2-3");
 			/*
-			 * 吏湲??ъ씤?곌? 媛由ы궎???뷀듃由ш? 諛몃━?쒗븯硫?			 * 洹몃냸??已볦븘蹂대궡?쇳븿
-			 * merge?댁꽌
-			 * ?ㅼ떆 ?
+			 * 지금 포인터가 가리키는 엔트리가 밸리드하면
+			 * 그놈을 쫓아보내야함
+			 * merge해서
+			 * 다시 씀
 			 */
-			//已볤꺼?섎뒗???⑥빞??			if(new_ppa == 0)
+			//쫓겨나는놈 써야함
+			if(new_ppa == 0)
 			{
-				//?뺤긽?곸씤 寃쎈줈
+				uart_printf("case2-3-1");
+				//정상적인 경로
 				new_ppa = assign_new_write_vpn(bank);
 			}
 
 			if(offset != (g_delta_pmt_pointer + 1) % NUM_MAX_DELTA_PAGES)
 			{
+				uart_printf("case2-3-2");
 				/*
-				 * 萸붽? 萸붽????섑빐??寃??꾨━?쇳뵾?먯씠瑜?媛붾떎 ?붾뜑??				 * delta 留ㅽ븨???ъ씤?곌? 諛붾뚯뼱 ?덉쓬
-				 * ?꾨쭏??gc?섎㈃???명?媛 已볤꺼????				 * ?명? 留ㅽ븨??諛붽씔 寃쎌슦??				 * 吏湲덇퍘 ?섎뜕嫄?洹몃깷 ?λ릺怨?				 * 泥섏쓬遺???ㅼ떆 ?쒖옉!
+				 * 뭔가 뭔가에 의해서 겟 프리피피에이를 갔다 왔더니
+				 * delta 매핑의 포인터가 바뀌어 있음
+				 * 아마도 gc하면서 델타가 쫓겨날 때
+				 * 델타 매핑을 바꾼 경우임
+				 * 지금껏 하던거 그냥 똥되고
+				 * 처음부터 다시 시작!
 				 */
 				goto GET_NEXT_DELTA_TABLE_SPACE_START;
 			}
 			else
 			{
+				uart_printf("case2-3-3");
 				/*
-				 * 湲곗〈???덈뜕??癒몄??댁꽌 已볦븘?댁옄~
+				 * 기존에 있던놈 머지해서 쫓아내자~
 				 */
 				UINT32 vblock   = new_ppa / PAGES_PER_BLK;
 				UINT32 page_num = new_ppa % PAGES_PER_BLK;
@@ -1471,29 +1526,40 @@ static void write_to_delta(UINT32 const bank, UINT32 const lpa, UINT32 const buf
 	UINT32 delta_ppn;
 	UINT32 vblock, voffset;
 
-	//?뺤텞???명????ъ씠利?李얠쓬
+ASSERT(buf_addr >= DRAM_BASE && buf_addr < (DRAM_BASE + DRAM_SIZE));
+	//압축된 델타의 사이즈 찾음
 	cs = read_dram_16(buf_addr);
 
-	//?명? 踰꾪띁媛 ?뺤텞???명?瑜??ｌ쓣 ???덉쑝硫?	if(is_remain_delta_buffer(bank, cs))
+	//델타 버퍼가 압축된 델타를 넣을 수 있으면
+	if(is_remain_delta_buffer(bank, cs) != 0)
 	{
-		//?꾩옱 ??lpa媛 ?명? 踰꾪띁???덈뒗 lpa? 媛숈? 寃껋씠 ?덉쑝硫?		//?명? 踰꾪띁???덈뒗 lpa瑜?INVALID ?쒗궡.
+uart_printf("is_remain_delta_buffer");
+
+		//현재 쓸 lpa가 델타 버퍼에 있는 lpa와 같은 것이 있으면
+		//델타 버퍼에 있는 lpa를 INVALID 시킴.
 		data_cnt = read_dram_32(DELTA_BUF(bank));
+
 		for(i = 0; i < data_cnt; i++)
 		{
 			lpa_out = read_dram_32(buf_addr + (2 * i + 2) * sizeof(UINT32));
+
 			if(lpa_out == lpa)
 			{
-				mem_set_dram(buf_addr + (2 * i + 2) * sizeof(UINT32), INVAL, sizeof(UINT32));
+				write_dram_32(buf_addr + (2 * i + 2) * sizeof(UINT32), INVAL);
+				//mem_set_dram(buf_addr + (2 * i + 2) * sizeof(UINT32), INVAL, sizeof(UINT32));
 			}
+
 		}
 
 	}
-	else	//?명? 踰꾪띁媛 ?뺤텞???명?瑜?紐??ｌ쓬
+	else	//델타 버퍼가 압축된 델타를 못 넣음
 	{
-		//???섏씠吏 ?좊떦
+uart_printf("is_remain_delta_buffer not");
+
+		//새 페이지 할당
 		delta_ppn = assign_new_write_vpn(bank);
 
-		//?명? ?섏씠吏 ?댁쓽 lpn??????섏씠吏 留ㅽ븨 ?뚯씠釉붿쓽 delta_ppa?ㅼ쓣 諛붽퓭以?
+		//델타 페이지 내의 lpn에 대해 페이지 매핑 테이블의 delta_ppa들을 바꿔줌.
 		data_cnt = read_dram_32(DELTA_BUF(bank));
 		for(i = 0; i < data_cnt; i++)
 		{
@@ -1504,22 +1570,39 @@ static void write_to_delta(UINT32 const bank, UINT32 const lpa, UINT32 const buf
 				set_delta_ppa(offset_out, delta_ppn);
 			}
 		}
-		//nand page???!!
+		//nand page에 씀!!
 		vblock = get_pbn(delta_ppn);
 		voffset = get_offset(delta_ppn);
 		nand_page_program(bank, vblock, voffset, DELTA_BUF(bank));
 
-		//LPA BUF?먮룄 ?⑥쨲!!
+		//LPA BUF에도 써줌!!
 		set_lpn(bank, delta_ppn / PAGES_PER_BLK, INVAL);
 
-		//?명? 硫뷀?, offset 珥덇린???댁＜湲?		next_delta_meta[bank] = DELTA_BUF(bank) + sizeof(UINT32);
+		//델타 메타, offset, data수 초기화 해주기
+		next_delta_meta[bank] = DELTA_BUF(bank) + sizeof(UINT32);
 		next_delta_offset[bank] = DELTA_BUF(bank) + (1 + 2 * MAX_DELTAS_PER_PAGE) * sizeof(UINT32);
-
+		data_cnt = 0;
 	}
 
-	//delta?댁뿉 delta?⑥＜怨? lpn ???⑥＜怨? offset???⑥＜怨?	//?꾩뿉??怨듯넻?섎뒗 遺遺꾩씠??類?	write_dram_32(next_delta_meta[bank], next_delta_offset[bank]);
+	uart_printf("next_delta_meta[bank] : %x DB %x DB+DS %x", next_delta_meta[bank], DRAM_BASE, DRAM_BASE + DRAM_SIZE);
+	ASSERT(next_delta_meta[bank] >= DRAM_BASE && next_delta_meta[bank] < (DRAM_BASE + DRAM_SIZE));
+	//delta내에 delta써주고, lpn 도 써주고, offset도 써주고 data개수도 써주고
+	//위에서 공통되는 부분이라 뺌
+	write_dram_32(DELTA_BUF(bank), data_cnt + 1);
+	write_dram_32(next_delta_meta[bank], next_delta_offset[bank]);
 	write_dram_32(next_delta_meta[bank] + sizeof(UINT32), lpa);
-	mem_copy(next_delta_offset[bank], buf_addr, cs);
+
+
+	//UINT32 i;
+	UINT32 buf;
+	for(i = 0; i < cs; i = i + sizeof(UINT32))
+	{
+		buf = read_dram_32(buf_addr + i);
+		write_dram_32(next_delta_offset[bank] + i, buf);
+	}
+
+
+//	mem_copy(next_delta_offset[bank], buf_addr, ALIGN32(cs));
 	next_delta_meta[bank] = next_delta_meta[bank] + 2 * sizeof(UINT32);
 	next_delta_offset[bank] = next_delta_offset[bank] + (cs + sizeof(UINT32) - 1)/ sizeof(UINT32) * sizeof(UINT32);
 
@@ -1546,7 +1629,7 @@ static UINT32 find_delta_data(UINT32 const buf_ptr, UINT32 const lpa)		//find de
 	//buf_ptr = buf_ptr + sizeof(UINT32);			//lpa starts at buf_ptr + 32
 	UINT32 offset = buf_ptr + sizeof(UINT32);
 	UINT32 cnt = read_dram_32(buf_ptr);
-
+	uart_printf("find_delta_data : cnt : %d", cnt);
 	for(i = 0; i < cnt; i++)
 	{
 		if(lpa == read_dram_32(offset + sizeof(UINT32)))
@@ -1574,15 +1657,26 @@ static void _lzf_decompress (UINT32 const in_data_base, UINT32 const out_data_ba
 	UINT32 in_data = in_data_base + sizeof(UINT32);
 	UINT32 out_data = out_data_base;
 
-	cs = read_dram_16(in_data_base);
+	UINT32 decompsize;
 
+ASSERT(in_data_base >= DRAM_BASE && in_data_base < (DRAM_BASE + DRAM_SIZE));
+ASSERT(out_data_base >= DRAM_BASE && out_data_base < (DRAM_BASE + DRAM_SIZE));
+
+	cs = read_dram_16(in_data_base);
+uart_printf("decomp start");
 	for(i = 0; i < NUM_COMP_CLUSTER; i++)
 	{
 		//load meta & data
-		cs_cluster = read_dram_16(in_data);		//?먮즺?ш린 ?쎌쓬
+		cs_cluster = read_dram_16(in_data);		//자료크기 읽음
 		mem_copy(header1, in_data + sizeof(UINT32), ALIGN32(cs_cluster));	// copy compressed data to header1(SRAM)
 
-		ASSERT (lzf_decompress ((void*)header1, cs_cluster, (void*)header2, CLUSTER_SIZE) == CLUSTER_SIZE);
+		uart_printf("i : %d header1 : %x header2 : %x cs_cluster %d", i, header1, header2, cs_cluster);
+uart_printf("cs_cluster[%d] : %d", i, cs_cluster);
+
+		decompsize = lzf_decompress ((void*)header1, cs_cluster, (void*)header2, CLUSTER_SIZE);
+		uart_printf("decompsize %d", decompsize);
+		ASSERT(decompsize == CLUSTER_SIZE);
+		//ASSERT (lzf_decompress ((void*)header1, cs_cluster, (void*)header2, CLUSTER_SIZE) == CLUSTER_SIZE);
 
 		mem_copy(out_data, header2, CLUSTER_SIZE);		// copy decompressed data to out_data(DRAM)
 
@@ -1596,18 +1690,21 @@ static UINT32 _lzf_compress (UINT32 const in_data_base, UINT32 const out_data_ba
 {
 	UINT16 cs = 0;
 	UINT16 cs_cluster;
-	volatile UINT16 header1[CLUSTER_SIZE + 1];    //decompressed buffer
-	volatile UINT16 header2[CLUSTER_SIZE + 1];    //compressed buffer
+	volatile UINT8 header1[CLUSTER_SIZE + 1];    //decompressed buffer
+	volatile UINT8 header2[CLUSTER_SIZE + 1];    //compressed buffer
 	UINT32 i;
 	UINT32 in_data = in_data_base;
 	UINT32 out_data = out_data_base + sizeof(UINT32);	//front of out_data_base : cs
 
+ASSERT(in_data_base >= DRAM_BASE && in_data_base < (DRAM_BASE + DRAM_SIZE));
+ASSERT(out_data_base >= DRAM_BASE && out_data_base < (DRAM_BASE + DRAM_SIZE));
+uart_printf("comp start");
 	for(i = 0; i < NUM_COMP_CLUSTER; i++)
 	{
 		mem_copy(header1, in_data, CLUSTER_SIZE);
 
 		cs_cluster = lzf_compress ((void*)header1, CLUSTER_SIZE, (void*)&header2[1], CLUSTER_SIZE - 4);	//compressed size of one cluster
-
+uart_printf("cs_cluster[%d] : %d", i, cs_cluster);
 		//memory check
 		cs = cs + ALIGN32(cs_cluster) + sizeof(UINT32);			//cs = cs + compressed data length + header
 		if(cs >= MAX_COMPRESS_SIZE)
@@ -1630,16 +1727,29 @@ static UINT32 _lzf_compress (UINT32 const in_data_base, UINT32 const out_data_ba
 	return cs;
 }
 
-static UINT32 is_remain_delta_buffer(UINT32 const bank, UINT32 const cs)	//is remain in delta_buffer?
+static UINT32 is_remain_delta_buffer(UINT32 const bank, UINT16 const cs)	//is remain in delta_buffer?
 {
-	if((next_delta_offset[bank] - DELTA_BUF(bank) + cs) > BYTES_PER_PAGE)
-	{
-		return 0;
-	}
-	if(next_delta_meta[bank] / (2 * sizeof(UINT32)) == MAX_DELTAS_PER_PAGE)
-	{
-		return 0;
-	}
+	UINT32 result;
 
+	result = next_delta_offset[bank] - DELTA_BUF(bank);
+	result = result + cs;
+
+	uart_printf("result : %x", result);
+
+	
+	if(result > BYTES_PER_PAGE)
+	//if(next_delta_offset[bank] - DELTA_BUF(bank) > BYTES_PER_PAGE)
+	{
+		return 0;
+	}
+	result = next_delta_meta[bank] >> 3;
+	uart_printf("result : %x, MAX_DELTAS_PER_PAGE : %x", result, MAX_DELTAS_PER_PAGE);
+
+	result = (result == MAX_DELTAS_PER_PAGE);
+	if(result)
+	//if(next_delta_meta[bank] / (2 * sizeof(UINT32)) == MAX_DELTAS_PER_PAGE)
+	{
+		return 0;
+	}
 	return 1;
 }
